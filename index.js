b25eacff0d19c5b85e229322a4fa728c6392f6ba
CodeDog3 <jonbernnan800@gmail.com>
2023-05-17 19:23:54 -0400

    fixed transition and added beginning of portfolio page

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const portfolioBtn = document.querySelector(".portfolio-btn");
 const educationBtn = document.querySelector(".education-btn");
 const contactBtn = document.querySelector('.contact-link');
 const intro = document.querySelectorAll(".intro-dissapear");
+const portfolio = document.querySelector(".profile-screen");
 const text = document.querySelector(".text");
+const footer = document.querySelector(".footer");
 
 
 portfolioBtn.addEventListener("click", ()=>{
+
+    footer.style.opacity = '0';
+
+    setTimeout(()=>{
+        footer.style.display = 'none';
+    })
+
 
     intro.forEach(elem =>{
         elem.style.opacity = '0';
@@ -14,6 +23,20 @@ portfolioBtn.addEventListener("click", ()=>{
             elem.style.display = 'none';
         },1000)      
     });
+
+    setTimeout(()=>{
+        portfolio.style.display = 'block';
+    },2500);
+
+    setTimeout(()=>{
+
+        portfolio.style.opacity = '1';
+        portfolio.style.visibility = 'visible'
+        footer.style.position = 'relative'
+        footer.style.display = 'flex';
+        footer.style.opacity = '1';
+
+    },2600);
 
     setTimeout(()=>{
         text.style.bottom = '90%';
